5496794c816488bb650a5f40174ff24e6b8338bf
arunsahoo-xt <armodilo60@gmail.com>
2023-07-23 18:54:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var images = document.getElementsByClassName('categoryImg');
+for (var i = 0; i < images.length; i++) {
+    images[i].src = 'images/category/category (' + (i + 1) + ').jpeg';
+}
+
+
+var images = document.getElementsByClassName('itemImg');
+for (var i = 0; i < images.length; i++) {
+    images[i].src = 'images/best_deals/deals (' + (i + 1) + ').jpeg';
+}
+
+
+
+var images = document.getElementsByClassName('trendImg');
+for (var i = 0; i < images.length; i++) {
+    images[i].src = 'images/trends/trends (' + (i + 1) + ').jpg';
+}
+
